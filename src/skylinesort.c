a299c1b4230d59017731965cc585f5b6629672aa
Daniel <daniel.cussen@gmail.com>
2019-08-23 09:14:55 -0700

    improving variable names

--- src/skylinesort.c
+++ src/skylinesort.c
@@ -1,43 +1,41 @@
-void skylinesort(uint len, uint * v, uint * t, uint * a, uint * c){
-  //len length of
-  //v source vec
-  //t target vec
-  //a auxiliary vec
-  //c count vec
-  unsigned int e,i,j,p,m=MASK;
-  //e element
+void skylinesort(uint len, uint * vec, uint * tgt, uint * aux, uint * cnt){
+  //vec source vec
+  //tgt target vec
+  //aux auxiliary vec
+  //cnt count vec
+  unsigned int elt,i,j,pos,mask=MASK;
+  //elt element
   //i j indices
-  //p position
-  //m mask
+  //pos position
   for(i=len;i>0;i){
-    e=v[--i];
-    c[e]++;
-    p=e;
-    while(e>a[p]){
-      a[p]=e;
-      p=(p|(p+1))&m;
+    elt=vec[--i];
+    cnt[elt]++;
+    pos=elt;
+    while(elt>aux[pos]){
+      aux[pos]=elt;
+      pos=((pos+1)|pos)&mask;
     }
   }
   //gather
-  p=m;
-  for(i=len;i>c[0];i){
-    if(a[p]==0)
-      p=(((p+1)&p)-1)&m;
+  pos=mask;
+  for(i=len;i>cnt[0];i){
+    if(aux[pos]==0)
+      pos=(((pos+1)&pos)-1)&mask;
     else{
-      e=a[p];
-      p=a[p];
-      while(a[p]){
-	a[p]=0;
-	p|=p+1;
-	p&=m;
+      elt=aux[pos];
+      pos=aux[pos];
+      while(aux[pos]){
+	aux[pos]=0;
+	pos|=pos+1;
+	pos&=mask;
       }
-      for(j=0;j<c[e];j++)
-	t[--i]=e;
-      c[e]=0;
-      p=e-1;
+      for(j=0;j<cnt[elt];j++)
+	tgt[--i]=elt;
+      cnt[elt]=0;
+      pos=elt-1;
     }
   }
   while(i>0)
-    t[--i]=0;
-  c[0]=0;
+    tgt[--i]=0;
+  cnt[0]=0;
 }
